c31b9861175a4571dd4a3576f2b709878d9f396b
Evelyn Fifi Yeung <eyeung@seas.upenn.edu>
2014-09-13 00:13:03 -0400

    website can now run properly

--- openn/main.py
+++ openn/main.py
@@ -7,11 +7,23 @@ import os
 from flask import Flask
 from flask import render_template
 # from flask_pymongo import PyMongo
-
+from pymongo import MongoClient
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['MONGO_DBNAME'] = 'togethrdb'
 
+server = 'ds033750.mongolab.com'
+port = 33750
+db_name = 'opencourse'
+username = 'feef'
+password = 'p3nn4pps'
+
+conn = MongoClient(server, port)
+db = conn[db_name]
+db.authenticate(username, password)
+posts = db.posts
+print '\nNumber of posts', posts.find().count()
 
 @app.route('/')
 def landing():
