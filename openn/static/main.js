e3c0a462f43d77389b6c44801e9ba1b7a7a0b62d
Evelyn Fifi Yeung <eyeung@seas.upenn.edu>
2014-09-13 22:05:39 -0400

    logout clears chrome storage

--- openn/static/main.js
+++ openn/static/main.js
@@ -8,7 +8,13 @@ function logout() {
         location.reload();
     });
 }
-
+function setStorage(val) {
+    var customEvent = document.createEvent('Event');
+    customEvent.initEvent('removeUser', true, true);
+    hiddenDiv = document.getElementById('setUserRemoveDiv');
+    $('#setUserRemoveDiv').text(val);
+    hiddenDiv.dispatchEvent(customEvent);
+}
 $(function() {
     $('#logout').click( function() {
         logout();
