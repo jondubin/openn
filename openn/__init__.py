f7a9eb4c8f7da0c9b79b8b07e04dc9a51d9d373a
Evelyn Fifi Yeung <feefles@Evelyns-MacBook-Pro.local>
2014-09-15 22:48:24 -0400

    removing secret things from app

--- openn/__init__.py
+++ openn/__init__.py
@@ -4,26 +4,24 @@ from flask import render_template
 from flask.ext.bcrypt import Bcrypt
 from pymongo import MongoClient
 
+
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['MONGO_DBNAME'] = 'opencourse'
-app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 bcrypt = Bcrypt(app)
-app.config['REGUSER'] = 'UPENN_OD_emtz_1000643'
-app.config['REGPW'] = 'o448e5mnbutjcji5ufofo630ur'
-app.config['pcr_token'] = 'qL_UuCVxRBUmjWbkCdI554grLjRMPY'
+
+import openn.secret
 
 
 
 server = 'ds033750.mongolab.com'
 port = 33750
 db_name = 'opencourse'
-username = 'feef'
-password = 'p3nn4pps'
+# username = 'feef'
+# password = 'p3nn4pps'
 ##connect to database
 conn = MongoClient(server, port)
 mongo = conn[db_name]
-mongo.authenticate(username, password)
+mongo.authenticate(app.config['db_username'], app.config['db_password'])
 
 
 import openn.users
---
+++ openn/secret.py
@@ -0,0 +1,12 @@
+from openn import app
+
+
+app.config['MONGO_DBNAME'] = 'opencourse'
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
+app.config['REGUSER'] = 'UPENN_OD_emtz_1000643'
+app.config['REGPW'] = 'o448e5mnbutjcji5ufofo630ur'
+app.config['pcr_token'] = 'qL_UuCVxRBUmjWbkCdI554grLjRMPY'
+
+app.config['db_username'] = 'feef'
+app.config['db_password'] = 'p3nn4pps'
